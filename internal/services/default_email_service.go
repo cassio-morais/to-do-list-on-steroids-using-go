9453ba1189f7c2affd9157d31bf33fe5f00e47d1
Cássio Morais <cassio.morais@wake.tech>
2025-03-18 13:25:39 -0300

    feat: email improvement

--- internal/services/default_email_service.go
+++ internal/services/default_email_service.go
@@ -11,8 +11,8 @@ func NewDefaultEmailService() *DefaultEmailService {
 	return &DefaultEmailService{}
 }
 
-func (e *DefaultEmailService) Send(body interface{}, email string) {
+func (e *DefaultEmailService) Send(body any, email string) {
 	time.Sleep(4 * time.Second)
 
-	log.Printf("email to %s sent succesfully", email)
+	log.Printf("email with body: %+v sent to %s succesfully", body, email)
 }
